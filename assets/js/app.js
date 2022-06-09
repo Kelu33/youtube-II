aeb9febc94480a8861fd8366bb3a686df1914afc
unknown <kelu33@gmail.com>
2022-06-09 22:25:27 +0200

    add randomizer, TODO fill dataset

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,8 @@ const container = document.querySelector("main");
 fetch("data.json")
   .then((response) => response.json())
   .then((videoDataList) => {
+    videoDataList = shuffleArray(videoDataList);
     let video;
-
     for (let videoData of videoDataList) {
       video = new Video(
         videoData.src,
@@ -17,3 +17,17 @@ fetch("data.json")
     }
   })
   .catch((error) => console.log(error));
+
+function shuffleArray(array) {
+  let shuffled = [];
+  for (let element of array) {
+    shuffled.splice(getRandomArbitrary(0, shuffled.length + 1), 0, element);
+  }
+  return shuffled;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5]));
