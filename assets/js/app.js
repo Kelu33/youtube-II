e1a9fd94da408f0a1007ab207b209c713e155038
Lucas Baste <kelu33@gmail.com>
2022-06-13 12:24:55 +0200

    add search bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
 const container = document.querySelector("main");
+const form = document.querySelector('form');
+
+let filter;
+let href = window.location.href.split('?');
+if (href.length > 1) filter = href[1].split('=')[1].toLowerCase();
 
 fetch("data.json")
   .then((response) => response.json())
   .then((videoDataList) => {
     videoDataList = shuffleArray(videoDataList);
+    let videoList = [];
     let video;
+
     for (let videoData of videoDataList) {
       video = new Video(
         videoData.src,
@@ -13,10 +20,37 @@ fetch("data.json")
         videoData.account,
         videoData.info
       );
-      video.display(container);
+      videoList.push(video);      
     }
+    
+    if (filter) {
+      videoList = titleFilter(videoList, filter);
+    }    
+    for (let filtredVid of videoList) {
+      filtredVid.display(container);
+    }
+    
+    form.addEventListener('input', (e) => {
+      e.preventDefault();
+
+      let wrapList = document.querySelectorAll('.video-wrap');
+      for (let wrap of wrapList) {
+        container.removeChild(wrap);
+      }
+
+      let query = e.target.value.toLowerCase();
+      let filtredVidz = titleFilter(videoList, query);      
+
+      for (let video of filtredVidz) {
+        video.display(container);
+      }
+    })
   })
   .catch((error) => console.log(error));
+
+function titleFilter(array, filter) {
+  return array.filter(video => video.title.toLowerCase().includes(filter));
+}
 
 function shuffleArray(array) {
   let shuffled = [];
